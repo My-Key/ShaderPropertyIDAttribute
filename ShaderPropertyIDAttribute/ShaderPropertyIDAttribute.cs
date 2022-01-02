878ccdb57903c7eaf3fd75e7c152220d3636341c
Maciej <0my0key0@gmail.com>
2022-01-02 10:37:46 +0100

    ShaderPropertyIDAttribute - add documenting comments

--- ShaderPropertyIDAttribute/ShaderPropertyIDAttribute.cs
+++ ShaderPropertyIDAttribute/ShaderPropertyIDAttribute.cs
@@ -14,7 +14,6 @@ public abstract class ShaderPropertyIDAttributeBase : Attribute
 	public Type m_type;
 	public string m_reference;
 	
-
 	public ShaderPropertyIDAttributeBase(string reference, Type type)
 	{
 		m_reference = reference;
@@ -25,12 +24,26 @@ public abstract class ShaderPropertyIDAttributeBase : Attribute
 [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field)]
 public class MaterialPropertyIDAttribute : ShaderPropertyIDAttributeBase
 {
+	/// <summary>
+	/// Draw string as dropdown with properties from <paramref name="material"/>
+	/// </summary>
+	/// <param name="material">String that will be evaluated to <b>Material</b> reference.
+	/// <br/>
+	/// If set to <b>null or empty string</b> <i>first found <b>Material</b></i> from sibling fields and properties will be used</param>
+	/// <param name="type">Which properties of type should be listed in dropdown</param>
 	public MaterialPropertyIDAttribute(string material = null, Type type = Type.Any) : base(material, type) { }
 }
 
 [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field)]
 public class ShaderPropertyIDAttribute : ShaderPropertyIDAttributeBase
 {
+	/// <summary>
+	/// Draw string as dropdown with properties from <paramref name="shader"/>
+	/// </summary>
+	/// <param name="shader">String that will be evaluated to <b>Shader</b> reference.
+	/// <br/>
+	/// If set to <b>null or empty string</b> <i>first found <b>Shader</b></i> from sibling fields and properties will be used</param>
+	/// <param name="type">Which properties of type should be listed in dropdown</param>
 	public ShaderPropertyIDAttribute(string shader = null, Type type = Type.Any) : base(shader, type) { }
 }
 
@@ -38,6 +51,16 @@ public class ShaderPropertyIDAttribute : ShaderPropertyIDAttributeBase
 [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field)]
 public class RendererPropertyIDAttribute : ShaderPropertyIDAttributeBase
 {
+	/// <summary>
+	/// Draw string as dropdown with properties from <paramref name="renderer"/>
+	/// <br/>
+	/// Dropdown will use <b>first</b> material from <paramref name="renderer"/>.
+	/// To use other materials use <see cref="RendererPropertyIDParentListAttribute"/>
+	/// </summary>
+	/// <param name="renderer">String that will be evaluated to <b>Renderer</b> reference.
+	/// <br/>
+	/// If set to <b>null or empty string</b> <i>first found <b>Renderer</b></i> from sibling fields and properties will be used</param>
+	/// <param name="type">Which properties of type should be listed in dropdown</param>
 	public RendererPropertyIDAttribute(string renderer = null, Type type = Type.Any) : base(renderer, type) { }
 }
 
@@ -55,16 +78,28 @@ public interface IAttributeMatch<T> where T : ShaderPropertyIDAttributeBase {}
 
 public class MaterialsPropertyIDTypeOverrideAttribute : PropertyIDTypeOverrideBaseAttribute, IAttributeMatch<MaterialPropertyIDAttribute>
 {
+	/// <summary>
+	/// Override Type of <see cref="MaterialPropertyIDAttribute"/> in child fields and properties
+	/// </summary>
+	/// <param name="type">Which properties of type should be listed in dropdown</param>
 	public MaterialsPropertyIDTypeOverrideAttribute(ShaderPropertyIDAttributeBase.Type type) : base(type) { }
 }
 
 public class ShaderPropertyIDTypeOverrideAttribute : PropertyIDTypeOverrideBaseAttribute, IAttributeMatch<ShaderPropertyIDAttribute>
 {
+	/// <summary>
+	/// Override Type of <see cref="ShaderPropertyIDAttribute"/> in child fields and properties
+	/// </summary>
+	/// <param name="type">Which properties of type should be listed in dropdown</param>
 	public ShaderPropertyIDTypeOverrideAttribute(ShaderPropertyIDAttributeBase.Type type) : base(type) { }
 }
 
 public class RendererPropertyIDTypeOverrideAttribute : PropertyIDTypeOverrideBaseAttribute, IAttributeMatch<RendererPropertyIDAttribute>
 {
+	/// <summary>
+	/// Override Type of <see cref="RendererPropertyIDAttribute"/> in child fields and properties
+	/// </summary>
+	/// <param name="type">Which properties of type should be listed in dropdown</param>
 	public RendererPropertyIDTypeOverrideAttribute(ShaderPropertyIDAttributeBase.Type type) : base(type) { }
 }
 
@@ -72,7 +107,7 @@ public abstract class PropertyIDReferenceOverrideBaseAttribute : Attribute
 {
 	public string m_reference;
 	
-	public PropertyIDReferenceOverrideBaseAttribute(string reference = "")
+	public PropertyIDReferenceOverrideBaseAttribute(string reference = null)
 	{
 		m_reference = reference;
 	}
@@ -80,17 +115,42 @@ public abstract class PropertyIDReferenceOverrideBaseAttribute : Attribute
 
 public class MaterialPropertyIDMaterialOverrideAttribute : PropertyIDReferenceOverrideBaseAttribute, IAttributeMatch<MaterialPropertyIDAttribute>
 {
-	public MaterialPropertyIDMaterialOverrideAttribute(string material = "") : base(material) { }
+	/// <summary>
+	/// Override material of <see cref="MaterialPropertyIDAttribute"/> in child fields and properties
+	/// </summary>
+	/// <param name="material">String that will be evaluated to <b>Material</b> reference.
+	/// <br/>
+	/// If set to <b>null or empty string</b> <i>first found <b>Material</b></i> from sibling fields and properties will be used</param>
+	public MaterialPropertyIDMaterialOverrideAttribute(string material = null) : base(material) { }
 }
 
 public class ShaderPropertyIDShaderOverrideAttribute : PropertyIDReferenceOverrideBaseAttribute, IAttributeMatch<ShaderPropertyIDAttribute>
 {
-	public ShaderPropertyIDShaderOverrideAttribute(string shader = "") : base(shader) { }
+	/// <summary>
+	/// Override shader of <see cref="ShaderPropertyIDAttribute"/> in child fields and properties
+	/// </summary>
+	/// <param name="shader">String that will be evaluated to <b>Shader</b> reference.
+	/// <br/>
+	/// If set to <b>null or empty string</b> <i>first found <b>Shader</b></i> from sibling fields and properties will be used</param>
+	public ShaderPropertyIDShaderOverrideAttribute(string shader = null) : base(shader) { }
 }
 
 public class RendererPropertyIDRendererOverrideAttribute : PropertyIDReferenceOverrideBaseAttribute, IAttributeMatch<RendererPropertyIDAttribute>
 {
-	public RendererPropertyIDRendererOverrideAttribute(string renderer = "") : base(renderer) { }
+	/// <summary>
+	/// Override renderer of <see cref="RendererPropertyIDAttribute"/> in child fields and properties
+	/// </summary>
+	/// <param name="renderer">String that will be evaluated to <b>Renderer</b> reference.
+	/// <br/>
+	/// If set to <b>null or empty string</b> <i>first found <b>Renderer</b></i> from sibling fields and properties will be used</param>
+	public RendererPropertyIDRendererOverrideAttribute(string renderer = null) : base(renderer) { }
 }
 
+/// <summary>
+/// When added to list or array that contains string or class with fields or properties with <see cref="RendererPropertyIDAttribute"/>,
+/// dropdown will use material of corresponding index from Renderer:<br/>
+/// - properties and fields from index 0 will use material from index 0<br/>
+/// - properties and fields from index 1 will use material from index 1<br/>
+/// - etc.
+/// </summary>
 public class RendererPropertyIDParentListAttribute : RendererPropertyIDRendererOverrideAttribute { }
