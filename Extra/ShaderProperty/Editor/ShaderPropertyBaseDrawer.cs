a4602647d4627d8077dd4e2d9fd949750e693cd4
Maciej <0my0key0@gmail.com>
2022-01-02 10:03:43 +0100

    Add ShaderProperty and ShaderPropertyBaseDrawer

---
+++ Extra/ShaderProperty/Editor/ShaderPropertyBaseDrawer.cs
@@ -0,0 +1,11 @@
+﻿using Sirenix.OdinInspector.Editor;
+using UnityEngine;
+
+public class ShaderPropertyBaseDrawer<T> : OdinValueDrawer<T> where T : ShaderPropertyBase
+{
+	private InspectorProperty m_property;
+
+	protected override void Initialize() => m_property = Property.Children["m_property"];
+
+	protected override void DrawPropertyLayout(GUIContent label) => m_property.Draw(label);
+}
---
+++ Extra/ShaderProperty/ShaderProperty.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+[Serializable]
+public abstract class ShaderPropertyBase
+{
+	protected int m_id = -1;
+	
+	protected void PropertyChanged() => m_id = -1;
+	
+	protected abstract string Property { get; }
+
+	public static implicit operator int(ShaderPropertyBase property)
+	{
+		if (property.m_id < 0) 
+			property.m_id = Shader.PropertyToID(property.Property);
+
+		return property.m_id;
+	}
+}
+
+[Serializable]
+public class RendererProperty : ShaderPropertyBase
+{
+	[SerializeField, RendererPropertyID, OnValueChanged(nameof(PropertyChanged))]
+	private string m_property;
+
+	protected override string Property => m_property;
+}
+
+[Serializable]
+public class MaterialProperty : ShaderPropertyBase
+{
+	[SerializeField, MaterialPropertyID, OnValueChanged(nameof(PropertyChanged))]
+	private string m_property;
+
+	protected override string Property => m_property;
+}
+
+[Serializable]
+public class ShaderProperty : ShaderPropertyBase
+{
+	[SerializeField, ShaderPropertyID, OnValueChanged(nameof(PropertyChanged))]
+	private string m_property;
+
+	protected override string Property => m_property;
+}
